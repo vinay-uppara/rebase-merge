dfb61cd1d0c458fec9b4b41faf318cd3adc03fb6
uppara <upparavinay2003n@gmail.com>
2025-09-26 16:12:13 +0530

    addapp.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+# Initialize the Flask app
+app = Flask(__name__)
+
+# Define a route
+@app.route("/")
+def home():
+    return "Hello, World! This is my first Flask app."
+
+# Run the app
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
+
